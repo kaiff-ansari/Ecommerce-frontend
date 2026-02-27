6d9ad584bcbf1298c85bd4513e9fbccf66256310
kaiff-ansari <mohdkaif8672@gmail.com>
2026-02-27 15:53:13 +0530

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+const App = () => {
+  return (
+  
+      
+      <h1 className=
+      'flex items-center justify-center h-screen bg-gray-800 text-white text-4xl font-bold'>
+        Welcome
+      </h1>
+
+
+  
+  )
+}
+
+export default App
---
+++ src/components/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Products = () => {
+
+  
+
+
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
